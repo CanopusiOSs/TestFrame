b5f797708204a4351cf2ce80736a820af49062eb
Nirmal <nirmal.patel@canopusinfosystems.com>
2023-03-15 16:43:56 +0530

    init commit

---
+++ TestFrame/MyFrameWork.swift
@@ -0,0 +1,16 @@
+//
+//  MyFrameWork.swift
+//  TestFrame
+//
+//  Created by IOS on 15/03/23.
+//
+
+import Foundation
+
+public class MyFrameWork {
+    private init() {}
+    
+    public static func doSomething() -> String {
+        return "Pod is working."
+    }
+}
